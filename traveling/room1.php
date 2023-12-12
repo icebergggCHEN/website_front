e5c495d825c9217d0dd836a387733d593ba8b37a
unknown <a1231728@gmail.com>
2023-12-12 12:50:17 +0800

    t

---
+++ traveling/room1.php
@@ -0,0 +1,201 @@
+<?php include 'nav.html'; ?>
+
+
+    <section id="room" >
+        <div class="col-md-6 offset-md-3">
+            <div class="room_t">
+                <h1>景觀雙人房</h1>
+                <h4 id="need">(如需米奇米妮大型布偶請於訂房時告知)</h4>
+                <h4>View Double Room( Mickey and Minnie Family Room)</h4>
+                </br>
+                <h4>平日房價 NT$2100 /假日房價 NT$2400/過年房價NT$ 3280 </h4>
+                <h4>(於5/1起供應早餐) </h4>
+                <h4>Weekday Rate NT$2100 / Holiday Rate NT$2400/Chinese New Year's Rate MT$3280 </h4>
+
+
+            </div>
+            <div class="room_cont">
+                <li>本房型附獨立衛浴、冷氣、電視、吹風機、室內拖鞋、毛巾、清潔盥洗用品、熱水瓶、冰箱、WIFI</li>
+                <li>本房型可加床，最多可加1組(加床為沙發床，每床$300不含早餐)</li>
+                <li>為響應環境保護，本宿歡迎自備盥洗用具，如毛巾、浴巾、牙膏、牙刷、刮鬍刀、梳子等個人用品</li>
+                <li>本宿提供幼兒澡盆，歡迎提前電話預約</li>
+                <li>本宿無提供毛小孩住宿，敬請見諒</li>
+                <li>平日為星期日到星期四；假日為星期五、星期六(假日的前一天)</li>
+                
+                <div class="image-row">
+                    <div class="image-box">
+                        <div class="image-container" class="image-item">
+                            <!-- <a data-toggle="lightbox" href="https://static.wixstatic.com/media/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg/v1/fill/w_504,h_671,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg"> -->
+                                <img onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_2f7d284c6b5448df9ca0baddcee69c45~mv2.jpg/v1/fill/w_671,h_671,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/387ca2_2f7d284c6b5448df9ca0baddcee69c45~mv2.jpg" src="https://static.wixstatic.com/media/387ca2_2f7d284c6b5448df9ca0baddcee69c45~mv2.jpg/v1/fill/w_671,h_671,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/387ca2_2f7d284c6b5448df9ca0baddcee69c45~mv2.jpg" class="img-fluid" alt="Image 1">
+                            <!-- </a> -->
+                        </div>
+                        <div class="caption"> 景觀雙人房</div>
+                    </div>
+                    <div class="image-box">
+                        <div class="image-container" class="image-item">
+                            <!-- <a data-toggle="lightbox" href="https://static.wixstatic.com/media/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg/v1/fill/w_504,h_671,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg"> -->
+                                <img onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg/v1/fill/w_504,h_671,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg" src="https://static.wixstatic.com/media/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg/v1/fill/w_504,h_671,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_0327972fd6374272ae54b0b7c9c789f3~mv2.jpg" class="img-fluid" alt="Image 2">
+                            <!-- </a> -->
+                        </div>                       
+                        <div class="caption"> 景觀雙人房</div>
+                    </div>
+                    <div class="image-box">
+                        <div class="image-container" class="image-item">
+                            <img onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_69ebb2de155d42638179cb3665ff77c3~mv2.jpg/v1/fill/w_895,h_671,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/387ca2_69ebb2de155d42638179cb3665ff77c3~mv2.jpg" src="https://static.wixstatic.com/media/387ca2_69ebb2de155d42638179cb3665ff77c3~mv2.jpg/v1/fill/w_895,h_671,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/387ca2_69ebb2de155d42638179cb3665ff77c3~mv2.jpg" alt="Image 3">
+                        </div>
+                        <div class="caption"> 窗外山景</div>
+                    </div>
+                </div>
+                <div class="overlay" id="overlay" onclick="closeImage()">
+                        <span class="close-btn" onclick="closeImage()">&times;</span>
+                        <img src="" alt="Enlarged Image" id="enlarged-image">
+                </div>
+                
+ 
+            </div>
+   
+            
+        </div>   
+
+
+        <div class="col-md-6 offset-md-3">
+            <div class="room_t">
+                <h1>悠活雙人房</h1>
+                <!-- <h4 id="need">(如需米奇米妮大型布偶請於訂房時告知)</h4> -->
+                <h4>Leisure Double Room (Business Room)</h4>
+                </br>
+                <h4>平日房價 $1680 /假日房價 $2200/過年房價 $2880</h4>
+                <h4>(於5/1起供應早餐) </h4>
+                <h4>Weekday Rate $1680 / Holiday Rate $2200/New Year's Rate $2880 </h4>
+
+
+            </div>
+            <div class="room_cont">
+                <li>本房型附獨立衛浴、冷氣、電視、吹風機、室內拖鞋、毛巾、清潔盥洗用品、熱水瓶、冰箱、WIFI</li>
+                <li>本房型可加床，最多可加1組(加床為沙發床，每床$300不含早餐)</li>
+                <li>為響應環境保護，本宿歡迎自備盥洗用具，如毛巾、浴巾、牙膏、牙刷、刮鬍刀、梳子等個人用品</li>
+                <li>本宿提供幼兒澡盆，歡迎提前電話預約</li>
+                <li>本宿無提供毛小孩住宿，敬請見諒</li>
+                <li>平日為星期日到星期四；假日為星期五、星期六(假日的前一天)</li>
+                
+                <div class="image-row">
+                    <div class="image-box">
+                        <div class="image-container">
+                            <a data-toggle="lightbox" href="https://static.wixstatic.com/media/387ca2_e4f2cc4419034d3d8d6dec7c018add1e~mv2.jpg/v1/fill/w_274,h_244,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_e4f2cc4419034d3d8d6dec7c018add1e~mv2.jpg">
+                                <img src="https://static.wixstatic.com/media/387ca2_e4f2cc4419034d3d8d6dec7c018add1e~mv2.jpg/v1/fill/w_274,h_244,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_e4f2cc4419034d3d8d6dec7c018add1e~mv2.jpg" class="img-fluid" alt="Image 1">
+                            </a>
+                        </div>
+                        <div class="caption"> 悠活雙人房</div>
+                    </div>
+                    <div class="image-box">
+                        <div class="image-container">
+                            <a data-toggle="lightbox" href="https://static.wixstatic.com/media/387ca2_8dead75098be4671b2dace2a34de7575~mv2.jpg/v1/fill/w_498,h_664,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_8dead75098be4671b2dace2a34de7575~mv2.jpg">
+                                <img data-src="https://static.wixstatic.com/media/387ca2_8dead75098be4671b2dace2a34de7575~mv2.jpg/v1/fill/w_498,h_664,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_8dead75098be4671b2dace2a34de7575~mv2.jpg" src="https://static.wixstatic.com/media/387ca2_8dead75098be4671b2dace2a34de7575~mv2.jpg/v1/fill/w_498,h_664,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_8dead75098be4671b2dace2a34de7575~mv2.jpg" class="img-fluid" alt="Image 2">
+                            </a>
+                        </div>                       
+                        <div class="caption"> 悠活雙人房</div>
+                    </div>
+                    <div class="image-box">
+                        <div class="image-container">
+                            <a data-toggle="lightbox" href="https://static.wixstatic.com/media/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg/v1/fill/w_274,h_244,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg">
+                                <img data-src="https://static.wixstatic.com/media/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg/v1/fill/w_274,h_244,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg" src="https://static.wixstatic.com/media/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg/v1/fill/w_274,h_244,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg" class="img-fluid" alt="Image 2">
+                            </a>
+                            <!-- <img data-toggle="lightbox" data-src="https://static.wixstatic.com/media/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg/v1/fill/w_274,h_244,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_8ba4203f290f4ca38711fc66ca6a9c7e~mv2.jpg" alt="Image 3"> -->
+                        </div>
+                        <div class="caption"> 小吧檯</div>
+                    </div>
+                </div>
+ 
+            </div>
+    
+            
+        </div>  
+        <div class="bottom col-md-6 offset-md-3">
+            <h5>©地址：367苗栗縣三義鄉廣盛村八股路館前二巷1-10號(木雕博物館附近) </h5>
+            <h5>《No.1-10, Guanqian 2th Ln., Bagu Rd., Sanyi Township, Miaoli County 367, Taiwan (R.O.C.)》</h5>
+            <h5>PHONE訂房聯絡專線：0965-300-358訂房專線或0988-284-891 小新管家</h5>
+            <p>版權所有，歡迎連結，但不得摘錄，必要時請與『正在旅行民宿--背包客棧』連絡
+                Copyright © backpacker All Rights Reserved.  建議瀏覽器:1024 * 768解析度，IE6.0以上，FLASH6.0 以上版本</p>
+        </div> 
+    
+
+
+    </section>
+    <script>
+            function openImage(image) {
+                // console.log("Show more images");
+                const overlay = document.getElementById("overlay");
+                const enlargedImage = document.getElementById("enlarged-image");
+
+                enlargedImage.src = image.src;
+                overlay.style.display = "flex";
+            }
+
+            function closeImage() {
+                // console.log("Show more images");
+                const overlay = document.getElementById("overlay");
+                overlay.style.display = "none";
+            }
+            // const overlay = document.getElementById("overlay");
+            // overlay.addEventListener("click", function(event) {
+            //     if (event.target === overlay) {
+            //         closeImage();
+            //     }
+            // });
+
+            function showMore() {
+                console.log("Show more images");
+                const hiddenImages = document.querySelectorAll(".image-item.hidden");
+                hiddenImages.forEach(image => {
+                    image.classList.remove("hidden");
+                });
+
+                const show= document.getElementById("show");
+                show.style.display = "none";
+            }
+        </script>
+        <style>
+            .image-item {
+                position: relative;
+            }
+
+            .image-item img {
+                width: 100%;
+                max-height: 200px;
+                cursor: pointer;
+            }
+
+            .overlay {
+                display: none;
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(0, 0, 0, 0.8);
+                justify-content: center;
+                align-items: center;
+            }
+
+            .overlay img {
+                max-width: 90%;
+                max-height: 80%;
+            }
+
+            .close-btn {
+                position: absolute;
+                top: 10px;
+                right: 10px;
+                color: white;
+                cursor: pointer;
+                font-size: 50px;
+                margin-top: 50px;
+                /* justify-content: center; */
+                
+            }
+        </style>
+    </body>
+</html>
+
+
+
---
+++ traveling/room2.php
@@ -0,0 +1,285 @@
+<?php include 'nav.html'; ?>
+    
+
+        <section id="room" >
+            <div class="col-md-6 offset-md-3">
+                <div class="room_t">
+                    <h1>背包館房型</h1>
+                    <!-- <h4 id="need">(如需米奇米妮大型布偶請於訂房時告知)</h4> -->
+                    <h4>Backpack room</h4>
+                    </br>
+                    <h4>背包客床位不分平、假日，每人NT$600元。過年每人NT$800元 </h4>
+                    <h4>(於5/1起供應早餐) </h4>
+                    <h4>Backpacker beds are not divided into weekdays and holidays, One bed NT$600.One bed NT$800 during Chinese New Year . </h4>
+
+
+                </div>
+                <div class="room_cont">
+                    <div>
+                        <li>背包房型不提供早餐</li>
+                        <li>背包客房型不提供私人清潔用品，如毛巾、牙膏、牙刷、梳子、刮鬍刀、洗面乳等，訂房前請留意</li>
+                        <li>本宿有提供有提供洗髮精、沐浴乳、吹風機、拖鞋、脫水機及附鎖置物櫃等設施</li>
+                        <li>交誼廳開放使用時間為AM8:00至PM11:00</li>
+                        <li>背包館床位由管家安排，女生可指定女生3+1人房型</li>
+                        <li>本宿提供幼兒澡盆，歡迎提前電話預約</li>
+                        <li>本宿無提供毛小孩住宿，敬請見諒</li>
+                    </div>
+                    
+                    </br>
+                    </br>
+                
+
+                    <div class="image-grid">
+                        <div class="image-item" >
+                            <img onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_4c23756f27ef4903a4676107afb05fc4~mv2_d_4032_3024_s_4_2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_4c23756f27ef4903a4676107afb05fc4~mv2_d_4032_3024_s_4_2.jpg" alt="上下床鋪10人房(男女混宿)" onclick="openImage(this)">
+                            <div class="image-caption">
+                                上下床鋪10人房
+                            </div>
+                        </div>
+                        <div class="image-item" >
+                            <img  onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_b62b25113cbc40abb74ec0c6de31d86b~mv2_d_4032_3024_s_4_2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_b62b25113cbc40abb74ec0c6de31d86b~mv2_d_4032_3024_s_4_2.jpg" alt="六人房(男女混宿)" onclick="openImage(this)">
+                            <div class="image-caption">
+                                六人房
+                            </div>
+                        </div>
+                        <div class="image-item" >
+                            <img  onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_24d7a1c4ddeb4cd190d250cd3e0a453a~mv2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_24d7a1c4ddeb4cd190d250cd3e0a453a~mv2.jpg" alt="家庭房" onclick="openImage(this)">
+                            <div class="image-caption">
+                                家庭房
+                            </div>
+                        </div>
+                        <div class="image-item" >
+                            <img  onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_907872f20f8446afa762c4db106cddb9~mv2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_907872f20f8446afa762c4db106cddb9~mv2.jpg" alt="雙人房(男女混宿)" onclick="openImage(this)">
+                            <div class="image-caption">
+                                雙人房
+                            </div>
+                        </div>
+                        <div class="image-item" >
+                            <img  onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_02b53ece27c848f4be07582547af80ac~mv2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_02b53ece27c848f4be07582547af80ac~mv2.jpg" alt="雅房(男女混宿)" onclick="openImage(this)">
+                            <div class="image-caption">
+                                茶水間
+                            </div>
+                        </div>
+                        <div class="image-item" >
+                            <img  onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_7021850f2c4447a3aeb54d2b76fe89b8~mv2_d_3024_4032_s_4_2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_7021850f2c4447a3aeb54d2b76fe89b8~mv2_d_3024_4032_s_4_2.jpg" alt="四人房(男女混宿)" onclick="openImage(this)">
+                            <div class="image-caption">
+                                盥洗室
+                            </div>
+                        </div>
+                        <!-- <div class="image-item">
+                            <img src="sheep.png" alt="Image 1" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 1
+                            </div>
+                        </div>
+                        <div class="image-item">
+                            <img src="sheep.png" alt="Image 2" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 2
+                            </div>
+                        </div>
+                        <div class="image-item">
+                            <img src="sheep.png" alt="Image 1" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 3
+                            </div>
+                        </div>
+                        <div class="image-item">
+                            <img src="sheep.png" alt="Image 2" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 4
+                            </div>
+                        </div>
+                        <div class="image-item">
+                            <img src="sheep.png" alt="Image 1" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 5
+                            </div>
+                        </div> -->
+                        <!-- <div class="image-item">
+                            <img src="sheep.png" alt="Image 2" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 6
+                            </div>
+                        </div> -->
+                        <div class="image-item" >
+                            <img onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_db9b13b81bb542ce9d2474b1ff341b53~mv2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_db9b13b81bb542ce9d2474b1ff341b53~mv2.jpg" alt="Image 1" onclick="openImage(this)">
+                            <div class="image-caption">
+                                交誼廳
+                            </div>
+                        </div>
+                        <div class="image-item" >
+                            <img  onclick="openImage(this)" src="https://static.wixstatic.com/media/387ca2_dc9e3251fc5f4218946d6d61de3eba0c~mv2_d_2248_4000_s_2.jpg/v1/fill/w_254,h_201,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/387ca2_dc9e3251fc5f4218946d6d61de3eba0c~mv2_d_2248_4000_s_2.jpg" alt="Image 2" onclick="openImage(this)">
+                            <div class="image-caption">
+                            陽台
+                            </div>
+                        </div>
+                        <div class="image-item hidden">
+                            <img src="sheep.png" alt="Image 1" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 9
+                            </div>
+                        </div>
+                        <div class="image-item hidden">
+                            <img src="sheep.png" alt="Image 2" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 10
+                            </div>
+                        </div>
+                        <div class="image-item hidden">
+                            <img src="sheep.png" alt="Image 1" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 11
+                            </div>
+                        </div>
+                        <div class="image-item hidden">
+                            <img src="sheep.png" alt="Image 2" onclick="openImage(this)">
+                            <div class="image-caption">
+                                Image 12
+                            </div>
+                        </div>
+                        <!-- Add more image items here -->
+                    </div>
+
+                    <div class="overlay" id="overlay" onclick="closeImage()">
+                        <span class="close-btn" onclick="closeImage()">&times;</span>
+                        <img src="" alt="Enlarged Image" id="enlarged-image">
+                    </div>
+                    <div class="center-button ">
+                        <button class="btn" id="show" onclick="showMore()">Show More</button>
+                    </div>
+
+                
+                </div>
+
+                
+
+                
+    
+                
+            </div>   
+
+
+            
+    
+                
+            </div>  
+            <div class="bottom col-md-6 offset-md-3">
+                <h5>©地址：367苗栗縣三義鄉廣盛村八股路館前二巷1-10號(木雕博物館附近) </h5>
+                <h5>《No.1-10, Guanqian 2th Ln., Bagu Rd., Sanyi Township, Miaoli County 367, Taiwan (R.O.C.)》</h5>
+                <h5>PHONE訂房聯絡專線：0965-300-358訂房專線或0988-284-891 小新管家</h5>
+                <p>版權所有，歡迎連結，但不得摘錄，必要時請與『正在旅行民宿--背包客棧』連絡
+                    Copyright © backpacker All Rights Reserved.  建議瀏覽器:1024 * 768解析度，IE6.0以上，FLASH6.0 以上版本</p>
+            </div> 
+        
+
+
+        </section>
+        
+
+
+        <style>
+            li{
+                padding-bottom: -5px;
+            }
+            .image-grid {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            gap: 10px;
+            }
+
+            .image-item {
+                position: relative;
+                background-color: rgba(0, 0, 0, 0.2);
+            }
+
+            .image-item img {
+                width: 100%;
+                max-height: 200px;
+                cursor: pointer;
+            }
+
+            .overlay {
+                display: none;
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(0, 0, 0, 0.8);
+                justify-content: center;
+                align-items: center;
+            }
+
+            .overlay img {
+                max-width: 90%;
+                max-height: 80%;
+            }
+
+            .close-btn {
+                position: absolute;
+                top: 10px;
+                right: 10px;
+                color: white;
+                cursor: pointer;
+                font-size: 50px;
+                margin-top: 50px;
+                /* justify-content: center; */
+                
+            }
+
+            .hidden {
+                display: none;
+            }
+            /* Other styles... */
+            /* button{
+                margin-top: 50px;
+                display: flex;
+                justify-content: center;
+            } */
+            
+            .center-button {
+                display: flex;
+                justify-content: center;
+                margin-top: 40px; /* 调整顶部间距 */
+            }
+        </style>
+        
+        <script>
+            function openImage(image) {
+                // console.log("Show more images");
+                const overlay = document.getElementById("overlay");
+                const enlargedImage = document.getElementById("enlarged-image");
+
+                enlargedImage.src = image.src;
+                overlay.style.display = "flex";
+            }
+
+            function closeImage() {
+                // console.log("Show more images");
+                const overlay = document.getElementById("overlay");
+                overlay.style.display = "none";
+            }
+            // const overlay = document.getElementById("overlay");
+            // overlay.addEventListener("click", function(event) {
+            //     if (event.target === overlay) {
+            //         closeImage();
+            //     }
+            // });
+
+            function showMore() {
+                console.log("Show more images");
+                const hiddenImages = document.querySelectorAll(".image-item.hidden");
+                hiddenImages.forEach(image => {
+                    image.classList.remove("hidden");
+                });
+
+                const show= document.getElementById("show");
+                show.style.display = "none";
+            }
+        </script>
+
+    </body>
+</html>
+
+
